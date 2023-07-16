c1331bbdf3fbdf44f60936834ec00974a2675cbb
cwetz <122042771+cwetz@users.noreply.github.com>
2023-07-15 20:26:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function animateButton() {
+    let button = document.getElementById('demoButton');
 
+    button.style.padding = '20px 40px';
+    button.style.fontSize = '30px';
+}
+
+function freezePage() {
+    // Infinite loop will cause the browser to freeze
+    while (true) {}
+}
